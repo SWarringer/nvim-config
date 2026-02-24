4948908cc56a304e9c527a4a7f04eead3ffa3280
SWarringer <warringer.simon@gmail.com>
2026-02-24 20:57:10 +0100

    integrated telescope with lsp

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -45,5 +45,10 @@ return {
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
+
+    -- Telescope LSP mappings
+    vim.keymap.set('n', 'grr', builtin.lsp_references, { desc = "LSP References" })
+    -- vim.keymap.set('n', 'gri', builtin.lsp_implementations, { desc = "LSP Implementations" })
+
   end,
 }

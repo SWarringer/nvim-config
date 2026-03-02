76ce24b498cab503389a76d74692e4990824fc7a
Simon Warringer <simon.warringer@consultant.volvo.com>
2026-03-02 14:52:50 +0100

    Removed typr plugin

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -2,5 +2,4 @@ return {
   { "alex-popov-tech/store.nvim", dependencies = { "OXY2DEV/markview.nvim" }, opts = {},cmd = "Store"},
   { "tpope/vim-fugitive", event = "VeryLazy" },
   { "bngarren/checkmate.nvim", ft = "markdown", opts = {files = {"*.md","**/todo.md","project/todo.md","/absolute/path.md", }}},
-  { "nvzone/typr", dependencies = "nvzone/volt", opts = {}, cmd = { "Typr", "TyprStats" },}
 }

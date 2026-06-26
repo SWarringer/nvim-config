2fa03e2e64e06236537280c6f40b5f028a5cd0c6
Simon Warringer <simon.warringer@lumenradio.com>
2026-06-26 09:56:43 +0200

    Added bindings for git

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,24 +1,35 @@
 return {
   {
-    "tpope/vim-fugitive", event = "VeryLazy" 
+    "tpope/vim-fugitive",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>gs", "<cmd>Git<cr>",        desc = "Git status" },
+      { "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Git diff split" },
+      { "<leader>gl", "<cmd>Git log<cr>",    desc = "Git log" },
+      { "<leader>gp", "<cmd>Git push<cr>",   desc = "Git push" },
+      { "<leader>gP", "<cmd>Git pull<cr>",   desc = "Git pull" },
+    },
   },
   {
     "kdheepak/lazygit.nvim",
     lazy = false,
     cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
     },
     -- optional for floating window border decoration
     dependencies = {
-        "nvim-telescope/telescope.nvim",
-        "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
     },
     config = function()
-        require("telescope").load_extension("lazygit")
+      require("telescope").load_extension("lazygit")
     end,
   },
   {
@@ -31,12 +42,29 @@ return {
         virt_text_pos = "right_align",
       },
       signs = {
-        add = { text = "│" },
-        change = { text = "│" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
+        add          = { text = "│" },
+        change       = { text = "│" },
+        delete       = { text = "_" },
+        topdelete    = { text = "‾" },
         changedelete = { text = "~" },
       },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local map = function(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+        end
+
+        -- Hunk navigation
+        map("n", "]h", gs.next_hunk, "Next hunk")
+        map("n", "[h", gs.prev_hunk,  "Previous hunk")
+
+        -- Hunk operations
+        map("n", "<leader>gh", gs.stage_hunk,                "Stage hunk")
+        map("n", "<leader>gr", gs.reset_hunk,                "Reset hunk")
+        map("n", "<leader>gv", gs.preview_hunk,              "Preview hunk")
+        map("n", "<leader>gb", gs.blame_line,                "Blame line")
+        map("n", "<leader>gB", gs.toggle_current_line_blame, "Toggle inline blame")
+      end,
     },
   },
 }

95f3996fbe440a4ba9a6594f2767b6831b0ffd45
Simon Warringer <simon.warringer@lumenradio.com>
2026-09-08 15:38:32 +0200

    Update telescope plugin to use master branch and refine treesitter setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,22 +5,32 @@ return {
         lazy = false,
         build = ":TSUpdate",
         config = function()
-            require("nvim-treesitter").setup({
-                ensure_installed = {
-                    "lua",
-                    "c",
-                    "python",
-                    "rust",
-                    "markdown",
-                    "markdown_inline",
-                },
-                highlight = {
-                    enable = true,
-                },
-                indent = {
-                    enable = true,
-                },
-                auto_install = false,
+            local ensure_installed = {
+                "lua",
+                "c",
+                "python",
+                "rust",
+                "markdown",
+                "markdown_inline",
+                "vimdoc",
+            }
+
+            local ts = require("nvim-treesitter")
+            ts.setup()
+
+            -- On the `main` branch, `setup()` does NOT install parsers or enable
+            -- highlight/indent. We install parsers and wire up highlighting manually.
+            ts.install(ensure_installed)
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = { "lua", "c", "python", "rust", "markdown", "help" },
+                callback = function(args)
+                    -- Enable treesitter-based highlighting.
+                    pcall(vim.treesitter.start, args.buf)
+                    -- Enable treesitter-based indentation.
+                    vim.bo[args.buf].indentexpr =
+                        "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
             })
         end,
     },
